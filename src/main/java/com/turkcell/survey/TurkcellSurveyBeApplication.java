187e17cd1ec0de38ea2b8af850c03088e905527a
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2021-10-11 23:18:41 +0300

    project created.

---
+++ src/main/java/com/turkcell/survey/TurkcellSurveyBeApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkcellSurveyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkcellSurveyBeApplication.class, args);
+	}
+
+}
